64478b92441306ad81d4a07f5527e7b2d2d18f14
Miguel Lumapat <lamppot@protonmail.com>
2021-06-14 21:48:15 -0400

    Implement bare GraphQL server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
+{-# language DataKinds             #-}
+{-# language FlexibleContexts      #-}
+{-# language OverloadedStrings     #-}
+{-# language PartialTypeSignatures #-}
+{-# language PolyKinds             #-}
+{-# language ScopedTypeVariables   #-}
+{-# language TemplateHaskell       #-}
+{-# language TypeApplications      #-}
+{-# language TypeFamilies          #-}
+{-# language TypeOperators         #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+
 module Main where
 
-import Lib
+import           Data.Proxy
+
+import           Mu.GraphQL.Quasi
+import           Mu.GraphQL.Server
+import           Mu.Server
+
+graphql "ServiceDefinition" "schema.graphql"
+serverPort = 8000
+
+-- GraphQL App
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ "Starting GraphQL server on port " ++ show serverPort
+    runGraphQLAppQuery serverPort server (Proxy @"Query")
+
+type ServiceMapping = '[]
+
+server :: MonadServer m => ServerT ServiceMapping i ServiceDefinition m _
+server = resolver ( object @"Query" ( method @"hello" $ pure $ Just "Tada!!" ) )
